fb6fe1332b1992f945507dcea42c89b49a10daf5
eugeneoregan <eugeneoregan@yahoo.co.uk>
2025-03-11 12:07:37 +0000

    Initial commit

---
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.sampleapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sampleapp.ui.theme.SampleAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SampleAppTheme {
+                Column {
+                    Row {
+                        Text("Hello")
+                        Text("Goodbye")
+                    }
+
+                    Row {
+
+                        val count = remember { mutableStateOf(0) }
+// content that you want to make clickable
+                        Text(
+                            text = count.value.toString(),
+                            modifier = Modifier.clickable { count.value += 1 })
+
+                    }
+                }
+            }
+        }
+    }
+}
+
